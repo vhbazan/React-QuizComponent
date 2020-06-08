c705807bd2e95aace2099b5b928a3e6549b7b5d1
vbazanm <victor_bazan_milla@optum.com>
2020-06-08 22:35:37 +0100

    Add Quiz component and reder it

---
+++ src/Quiz.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+let quizData = require('./quiz_data.json');
+
+class Quiz extends Component {
+  render() {
+
+  return (
+    <div>Quiz</div>
+  )
+  }
+}
+
+export default Quiz;
