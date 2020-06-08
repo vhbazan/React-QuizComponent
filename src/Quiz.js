f2269ec9333bf00f5308052f4987f0b36e105eeb
vbazanm <victor_bazan_milla@optum.com>
2020-06-08 23:37:55 +0100

    Add QuizEnd component and show/hide logic

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
@@ -12,9 +13,11 @@ class Quiz extends Component {
   }
 
   render() {
-
+  const isQuizEnd = this.state.quiz_position-1 === quizData.quiz_questions.length;
   return (
+    
     <div>
+      { isQuizEnd ? <QuizEnd /> : null}
       <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
     </div>
   )
