fe599d8d3a5e6c246b27fb788f31fc90fbcb8918
vbazanm <victor_bazan_milla@optum.com>
2020-06-08 22:46:06 +0100

    Display first Quiz question

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,10 +3,20 @@ import React, { Component } from 'react';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { quiz_position: 1}
+  }
+
   render() {
 
   return (
-    <div>Quiz</div>
+    <div>
+      <div className="QuizQuestion">
+        {quizData.quiz_questions[0].instruction_text }
+      </div>
+    </div>
   )
   }
 }
