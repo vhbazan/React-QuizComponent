b7f96bd555c2e2c73df13058a765a00b37f9f815
vbazanm <victor_bazan_milla@optum.com>
2020-06-08 23:41:26 +0100

    Display QuizQuestion if quiz is not finished

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,8 +17,9 @@ class Quiz extends Component {
   return (
     
     <div>
-      { isQuizEnd ? <QuizEnd /> : null}
-      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
+      { isQuizEnd ? <QuizEnd /> : 
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
+      }
     </div>
   )
   }
