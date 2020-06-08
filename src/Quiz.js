d3945b31f769a2e1c5517d3338a8095ad1ec9646
vbazanm <victor_bazan_milla@optum.com>
2020-06-09 00:15:58 +0100

    Reset quiz state

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,7 +17,7 @@ class Quiz extends Component {
   return (
     
     <div>
-      { isQuizEnd ? <QuizEnd /> : 
+      { isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> : 
         <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
                       showNextQuestionHandler={this.showNextQuestion.bind(this)}
         />
@@ -29,6 +29,12 @@ class Quiz extends Component {
     this.setState({
       quiz_position: this.state.quiz_position + 1
       
+    })
+  }
+
+  handleResetClick() {
+    this.setState({
+      quiz_position: 1
     })
   }
 }
